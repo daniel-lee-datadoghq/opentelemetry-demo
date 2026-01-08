8be087260795cf96de42ef65b670b7c879d05944
daniel-lee-datadoghq <daniel.lee@datadoghq.com>
2026-01-08 15:49:38 -0500

    - remove RUM client token - update docker compose file

--- src/frontend/utils/datadog-init.tsx
+++ src/frontend/utils/datadog-init.tsx
@@ -5,8 +5,8 @@ import { datadogRum } from '@datadog/browser-rum';
 
 if (typeof window !== 'undefined') {
   datadogRum.init({
-    applicationId: '384e96c9-a901-45b6-a5c2-5351ddd52495',
-    clientToken: 'pub634e9f5eb80df058d008db0e58a3fb6d',
+    applicationId: '<DD_APP_ID>',
+    clientToken: '<DD_CLIENT_TOKEN>',
     site: 'datadoghq.com',
     allowedTracingUrls: [
       /^http:\/\/[^\/]+\:8080\/api/
